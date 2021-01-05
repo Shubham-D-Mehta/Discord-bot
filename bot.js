6883d3608e7189b32c968740472bf4318669c7c4
Shubham Mehta <69765380+Shubham-D-Mehta@users.noreply.github.com>
2021-01-04 22:00:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,58 +1,149 @@
-const Discord = require('discord.js');
-const botsettings = require('./botsettings.json');
-const bot= new Discord.Client({disableEveryone: true});
-// var fs=require('fs');
-
-// bot.on("ready", () =>{
-//   console.log(`${bot.user.username}! is up!!!!!!!!!!`);
-//   bot.user.setActivity("Why is sahil here");
-// });
-bot.on("message", async message => {
-    if(message.author.bot || message.channel.type === "dm") return;
-
-    let prefix = botsettings.prefix;
-    let messageArray = message.content.split(" ")
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-
-    if(cmd === `${prefix}hi`){
-        return message.reply("whats up")
+require('dotenv').config();
+const { Client, Message, MessageMentions, MessageEmbed } = require('discord.js');
+const client = new Client({
+    partials:['MESSAGE', 'REACTION']
+});
+const PREFIX = process.env.TOKEN;
+let reminder_count=0;
+client.on('ready', () =>{
+    console.log(`${client.user.username} has logged in`)
+    client.user.setActivity('!help', { type: 'PLAYING' });
+});
+client.on('message', (message)=>{
+    if(message.author.bot) return;
+    if(message.content === 'Hello'){
+        message.channel.send(`${message.author}`);
     }
-    if(cmd === `${prefix}sir`)
-    {
-      return message.channel.send("a e i o u",{tts:true}) 
+    if(message.content.startsWith(PREFIX)){
+        const [command, ...args]= message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/);
+        if(command==='metz'){
+            var exampleEmbed = new MessageEmbed()
+            .setColor('#13cbd8')
+            .setTitle('List of commands:')
+            .setAuthor('Metz bot')
+            .addFields(
+            { name: '!kick', value: 'To kick a user' },
+            { name: '!remind', value: 'Reminds a user or yourself with an optional message'},
+            { name: '!play?', value: 'Pings active people in the group'},
+            )
+            .setTimestamp()
+            .setFooter('made by Metz');
+
+            return message.channel.send(exampleEmbed);
+        }
+        if(command==='kick'){
+            if(args.length===0 ) return message.reply("Who tf you wanna kick");
+            if(message.author.username==='metz'){
+                var member= message.guild.members.cache.get(args[0]);
+                if(member){
+                   message.channel.send("Woah calm down man");
+                }
+                else message.channel.send("Member not found");
+            }
+            else return message.channel.send("You cant do this :)");
+        }
+        if(command === 'remind'){
+            if(reminder_count>3) return message.channel.send("Reminder limit reached");
+            if(args.length == 0)
+            return message.channel.send("Improper use");
+            if (args[0].startsWith('<@') && args[0].endsWith('>')) {
+                var remind_message="";
+                if(args[2] != undefined){
+                    var i=2;
+                    while(args[i] != undefined){
+                        remind_message = remind_message.concat(args[i]).concat(' ');
+                        i+=1;
+                    }
+                }
+                args[0] = args[0].slice(2, -1);
+        
+                if (args[0].startsWith('!')) {
+                    args[0] = args[0].slice(1);
+                } 
+                var user=message.guild.members.cache.get(args[0]);
+                if(args[1].endsWith('h')){
+                    var time=parseInt(args[1]);
+                    if(time>5)
+                        return message.channel.send("Time cannot be more than 5 Hours right now :(");
+                    else
+                    setTimeout(() => {
+                        reminder_count-=1;
+                        return message.channel.send(`This is a reminder ${user}: ${remind_message}`);
+                    }, time*60*60*1000);
+                }
+                else if(args[1].endsWith('m')){
+                    var time=parseInt(args[1]);
+                    if(time>60)
+                        return message.channel.send(`Just use hours then 😀`);
+                    setTimeout(() => {
+                        reminder_count-=1;
+                        return message.channel.send(`This is a reminder ${user}: ${remind_message}`);
+                    }, time*60*1000);
+                }
+                if(args[1].endsWith('h') || args[1].endsWith('m'))
+                    reminder_count+=1;
+            }
+            else{
+                var remind_message="";
+                if(args[1] != undefined){
+                    var i=1;
+                    while(args[i] != undefined){
+                        remind_message = remind_message.concat(args[i]).concat(' ');
+                        i+=1;
+                    }
+                }
+                if(args[0].endsWith('h')){
+                    var time=parseInt(args[0]);
+                    if(time === 0) return;
+                    if(time>5)
+                        return message.channel.send("Time cannot be more than 5 Hours right now :(");
+                    else
+                    setTimeout(() => {
+                        reminder_count-=1;
+                        return message.channel.send(`This is a reminder ${user}: ${remind_message}`);
+                    }, time*60*60*1000);
+                }
+                else if(args[0].endsWith('m')){
+                     var time=parseInt(args[0]);
+                    if(time === 0)
+                        return;
+                    if(time>60)
+                        return message.channel.send(`Just use hours then 😀`);
+                    setTimeout(() => {
+                        reminder_count-=1;
+                        return message.channel.send(`This is a reminder ${message.author}: ${remind_message}`);
+                    }, time*60*1000);
+                }
+                if(args[0].endsWith('h') || args[0].endsWith('m'))
+                    reminder_count+=1;   
+            }
+        }
+         if(command === 'play?'){
+             if(message.member.user.id.toString() === '303202443947409408'){
+                //Metz is invoking
+                return message.channel.send(`${ message.guild.members.cache.get("299227567288877056") } ${ message.guild.members.cache.get("281997835371675658") }  ?`);
+             }
+             if(message.member.user.id.toString() === '299227567288877056'){
+                //kumo is invoking
+                return message.channel.send(`${ message.guild.members.cache.get("303202443947409408") } ${ message.guild.members.cache.get("281997835371675658") }  ?`);
+             }
+             if(message.member.user.id.toString() === '281997835371675658'){
+                //kanra is invoking
+                return message.channel.send(`${ message.guild.members.cache.get("299227567288877056") } ${ message.guild.members.cache.get("303202443947409408") }  ?`);
+             }
+             return message.channel.send("No one wants to play with you");
+         }
+         if(command === 'hi'){
+             return message.channel.send("Hello");
+         }
     }
-    if(cmd === `${prefix}who`)
-    {
-      return message.channel.send("sahil is the imposter",{tts:true})
-    }
-    if(cmd === `${prefix}dead`)
-    {
-      return message.channel.send("https://tenor.com/bpP0N.gif")
-    }
+});
 
-    if(cmd === `${prefix}sus`){
-      return message.channel.send("https://tenor.com/tQCR.gif");
-    }
+client.on('messageReactionAdd',(reaction, user)=>{
+    console.log("Reacted");
+});
 
-    if(cmd === `${prefix}commands`){
-      return message.channel.send("List of commands\n1. -hi (Say hello to the bot)\n2.  -sir (Says something fun)\n3.  -who (The obvious imposter)\n4.  -dead\n5.  -sus\n6.  -messages (Tells you how many messages you have posted)");
-    }
-
-    // if(!userData[message.member.id]) userData[message.member.id]={
-    //   messagesSent: 0
-    // }
- 
-    // userData[message.member.id].messagesSent++;
-
-    // fs.writeFile('storage/userData.json',JSON.stringify(userData), (err) => {
-    //   if(err) console.error()
-    // });
-
-    // if(cmd === `${prefix}messages`){
-    //   return message.reply("You have sent "+userData[message.member.id].messagesSent+" messages.")
-    // }
-
-})
-
-bot.login(process.env.BOT_TOKEN)
+client.login(process.env.DISCORDJS_BOT_TOKEN); 
